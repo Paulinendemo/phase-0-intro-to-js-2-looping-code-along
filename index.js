d147f6070f1cd11b85fda6e8d4a28c15a46fe9e3
Paulinendemo <pauline.ndemo@student.moringaschool.com>
2022-08-04 08:25:44 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+const countDown = (number) => {
+    let n = number;
+    while (n >= 0){
+        console.log(n--)
+    }
+}
+
+function writeCards(names, content){
+    const messages = []
+
+    for(let a = 0; a < names.length; a++){
+        const message = `Thank you, ${names[a]}, for the wonderful ${content} gift!`
+        messages.push(message)
+    }
+
+    
+
+    return messages;
+}
